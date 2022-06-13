a69fe2ccee5b03a69e79957fe617ef2735e82323
Saviorov <saviorov@gmail.com>
2022-06-13 18:04:06 +0300

    added fixedNavbar, arrowTop, actualYearDate.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// SLIDER
 $(function () {
     $('.design-slider').slick({
         slidesToShow: 4,
@@ -15,3 +16,43 @@ $(function () {
         ]
     });
 });
+
+
+// modal-btn
+// const modalBtn = document.querySelector(".modal-btn");
+// const modal = document.querySelector(".modal-overlay");
+// const closeBtn = document.querySelector(".close-btn");
+
+// modalBtn.addEventListener("click", function () {
+//     modal.classList.add("open-modal");
+// });
+// closeBtn.addEventListener("click", function () {
+//     modal.classList.remove("open-modal");
+// });
+
+
+
+// FIXED NAVBAR
+const navbar = document.querySelector('.header__top');
+const topLink = document.querySelector('.top-link');
+
+window.addEventListener('scroll', function () {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if (scrollHeight > navHeight) {
+        navbar.classList.add('fixed-nav');
+        navbar.classList.add('fixed-color');
+    } else {
+        navbar.classList.remove('fixed-nav');
+    };
+
+    if (scrollHeight > 500) {
+        topLink.classList.add('show-link')
+    } else {
+        topLink.classList.remove('show-link')
+    };
+});
+
+// ACTUAL YEAR
+const date = document.getElementById('date');
+date.innerHTML = new Date().getUTCFullYear();
